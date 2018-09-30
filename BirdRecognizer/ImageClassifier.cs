5dd9b71e8e8fe14b9a1b1c6bf2548b138a08656f
Andy Butland <abutland@zonedigital.com>
2018-09-30 20:13:13 +0200

    Implemented bird recognizer using exported custom vision model

---
+++ BirdRecognizer/ImageClassifier.cs
@@ -0,0 +1,81 @@
+﻿namespace BirdRecognizer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using Android.App;
+    using Android.Graphics;
+    using Org.Tensorflow.Contrib.Android;
+
+    public class ImageClassifier
+    {
+        private readonly List<string> _labels;
+        private readonly TensorFlowInferenceInterface _inferenceInterface;
+
+        public ImageClassifier()
+        {
+            const string ModelFileName = "model.pb";
+            const string LabelsFileName = "labels.txt";
+
+            var assets = Application.Context.Assets;
+
+            _inferenceInterface = new TensorFlowInferenceInterface(assets, ModelFileName);
+
+            using (var sr = new StreamReader(assets.Open(LabelsFileName)))
+            {
+                var content = sr.ReadToEnd();
+                _labels = LoadLabels(content);
+            }
+        }
+
+        private static List<string> LoadLabels(string content)
+        {
+            return content.Split('\n')
+                .Select(s => s.Trim())
+                .Where(s => !string.IsNullOrEmpty(s))
+                .ToList();
+        }
+
+        public ImageClassificationResult RecognizeImage(Bitmap bitmap)
+        {
+            const string InputName = "Placeholder";
+            const string OutputName = "loss";
+            const int InputSize = 227;
+            const int ColorDimensions = 3;  // R,G,B
+
+            var outputNames = new[] { OutputName };
+            var floatValues = GetBitmapPixels(bitmap, InputSize, ColorDimensions);
+            var outputs = new float[_labels.Count];
+
+            _inferenceInterface.Feed(InputName, floatValues, 1, InputSize, InputSize, ColorDimensions);
+            _inferenceInterface.Run(outputNames);
+            _inferenceInterface.Fetch(OutputName, outputs);
+
+            var results = outputs
+                .Select((x, i) => new ImageClassificationResult(_labels[i], x))
+                .ToList();
+
+            return results.OrderByDescending(t => t.Probability).First();
+        }
+
+        private static float[] GetBitmapPixels(Bitmap bitmap, int inputSize, int colorDimensions)
+        {
+            var floatValues = new float[inputSize * inputSize * colorDimensions];
+
+            using (var scaledBitmap = Bitmap.CreateScaledBitmap(bitmap, inputSize, inputSize, false))
+            {
+                using (var resizedBitmap = scaledBitmap.Copy(Bitmap.Config.Argb8888, false))
+                {
+                    var intValues = new int[inputSize * inputSize];
+                    resizedBitmap.GetPixels(intValues, 0, resizedBitmap.Width, 0, 0, resizedBitmap.Width, resizedBitmap.Height);
+                    resizedBitmap.Recycle();
+                }
+
+                scaledBitmap.Recycle();
+            }
+
+            return floatValues;
+        }
+    }
+}
--- BirdRecognizer/MainActivity.cs
+++ BirdRecognizer/MainActivity.cs
@@ -1,52 +1,97 @@
 ﻿namespace BirdRecognizer
 {
     using System;
+    using System.Threading.Tasks;
     using Android.App;
+    using Android.Graphics;
     using Android.OS;
-    using Android.Support.Design.Widget;
     using Android.Support.V7.App;
+    using Android.Util;
     using Android.Views;
     using Android.Widget;
+    using Plugin.CurrentActivity;
+    using Plugin.Media;
+    using Plugin.Media.Abstractions;
+    using Plugin.Permissions;
+    using Permission = Android.Content.PM.Permission;
 
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private readonly ImageClassifier _imageClassifier = new ImageClassifier();
+
+        private Button _selectPhotoButton;
+        private ImageView _photoView;
+        private TextView _resultLabel;
+        private ProgressBar _progressBar;
+
+        public Android.Support.V7.Widget.Toolbar Toolbar { get; set; }
+
+        private Button SelectPhotoButton
+            => _selectPhotoButton ?? (_selectPhotoButton = FindViewById<Button>(Resource.Id.select_photo_button));
+        
+        private ImageView PhotoView
+            => _photoView ?? (_photoView = FindViewById<ImageView>(Resource.Id.photo));
+        
+        private TextView ResultLabel
+            => _resultLabel ?? (_resultLabel = FindViewById<TextView>(Resource.Id.result_label));
+        
+        private ProgressBar ProgressBar
+            => _progressBar ?? (_progressBar = FindViewById<ProgressBar>(Resource.Id.progressbar));
+        
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            SetContentView(Resource.Layout.activity_main);
+            CrossCurrentActivity.Current.Activity = this;
 
-            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
-            SetSupportActionBar(toolbar);
+            SetContentView(Resource.Layout.main);
 
-            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
-        }
-
-        public override bool OnCreateOptionsMenu(IMenu menu)
-        {
-            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
-            return true;
-        }
-
-        public override bool OnOptionsItemSelected(IMenuItem item)
-        {
-            int id = item.ItemId;
-            if (id == Resource.Id.action_settings)
+            Toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            if (Toolbar != null)
             {
-                return true;
+                SetSupportActionBar(Toolbar);
             }
 
-            return base.OnOptionsItemSelected(item);
+            SupportActionBar.SetDisplayHomeAsUpEnabled(false);
+            SupportActionBar.SetHomeButtonEnabled(false);
+
+            SelectPhotoButton.Click += TakePhotoButtonClick;
         }
 
-        private void FabOnClick(object sender, EventArgs eventArgs)
+        private async void TakePhotoButtonClick(object sender, EventArgs e)
         {
-            View view = (View)sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+            if (!CrossMedia.Current.IsTakePhotoSupported)
+            {
+                Toast.MakeText(ApplicationContext, "Cannot select photos from the device.", ToastLength.Long).Show();
+                return;
+            }
+
+            SelectPhotoButton.Enabled = false;
+            ProgressBar.Visibility = ViewStates.Visible;
+
+            try
+            {
+                var image = await CrossMedia.Current.PickPhotoAsync(new PickMediaOptions { PhotoSize = PhotoSize.Medium });
+                var bitmap = await BitmapFactory.DecodeStreamAsync(image.GetStreamWithImageRotatedForExternalStorage());
+
+                PhotoView.SetImageBitmap(bitmap);
+
+                var result = await Task.Run(() => _imageClassifier.RecognizeImage(bitmap));
+                ResultLabel.Text = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("BirdRecognizer", ex.ToString());
+            }
+            finally
+            {
+                SelectPhotoButton.Enabled = true;
+                ProgressBar.Visibility = ViewStates.Invisible;
+            }
         }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+            => PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
-
